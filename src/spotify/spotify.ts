a72d5c9f13f1f890fe35e21c2564d144d8faec1b
djsavvy <sraghuvanshi@college.harvard.edu>
2022-08-01 20:53:19 -0700

    add note about future blocking approach instead of polling on macos

---
+++ src/spotify/spotify.ts
@@ -0,0 +1,3 @@
+// For now, we just use a polling implementation because it's simplest. However, in the future, we
+// can try a blocking approach (at least on macOS), akin to
+// https://github.com/laaksomavrick/menu-bar-for-spotify/blob/master/Spotify%20Helper/Services/SpotifyEventListener.swift
