ceff85aed8766a855870952c501e9d807d74d3de
djsavvy <sraghuvanshi@college.harvard.edu>
2022-08-14 10:07:23 -0700

    create trivial SongInfo struct

--- src/spotify/spotify.ts
+++ src/spotify/spotify.ts
@@ -1,3 +1,13 @@
 // For now, we just use a polling implementation because it's simplest. However, in the future, we
 // can try a blocking approach (at least on macOS), akin to
 // https://github.com/laaksomavrick/menu-bar-for-spotify/blob/master/Spotify%20Helper/Services/SpotifyEventListener.swift
+
+export type SongInfo = {
+  Title: string;
+  Artist: string;
+  AlbumArtURL: string;
+} | null;
+
+export function GetCurrentSongInfo(): SongInfo {
+  return null;
+}
